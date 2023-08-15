2017b4b3cc17ecb610d2f2d4b59ff134ff228754
ayushsaraf <ayushsaraf.saraf15@gmail.com>
2023-08-15 18:26:27 +0530

    silly

--- dashboard/asgi.py
+++ dashboard/asgi.py
@@ -14,3 +14,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
 
 application = get_asgi_application()
+
+app = application
