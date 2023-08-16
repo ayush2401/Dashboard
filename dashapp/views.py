03afeb1a43e623d0115601e737a70acd858ef6ad
ayushsaraf <ayushsaraf.saraf15@gmail.com>
2023-08-16 13:34:39 +0530

    finaly

--- dashapp/views.py
+++ dashapp/views.py
@@ -22,6 +22,7 @@ def index(request):
     likeli = {}
     rel_like = {}
     year = {}
+    intensity = {}
 
     for i in database.find():
         if i["sector"] == "Energy" and i["topic"] != '':
@@ -48,6 +49,15 @@ def index(request):
             else:
                 likeli[i['sector']] = [(int)(i['likelihood']) , 1]
         
+        if i['intensity'] != '' and i['sector'] != '':
+            if i['sector'] in intensity:
+                value = intensity[i['sector']]
+                value[0] += (int)(i['intensity'])
+                value[1] += 1
+                intensity[i['sector']] = value
+            else:
+                intensity[i['sector']] = [(int)(i['intensity']) , 1]
+        
 
         val = i['start_year']
         if val != '':
@@ -58,24 +68,35 @@ def index(request):
                 year[val] += 1
 
 
+    for key , values in intensity.items():
+        v1 , v2 = values
+        avg = round(v1/v2 , 2)
+        intensity[key] = avg
+
+
+    intensity = OrderedDict(sorted(intensity.items() , key=lambda x:x[1]))
     year = OrderedDict(sorted(year.items()))
 
-    mostrel , relval = '' , 0
+    print(intensity)
+
+    most , mval = ['',''] , [0 , 0]
     cat1 , cat2 = [] , []
-    sum , tot = 0 , 0
+    sum , tot = 0 , len(relevance)
+
     for key , values in relevance.items():
         v1 , v2 = values
-        avg1 = v1 / v2
-        if avg1 > relval:
-            relval = avg1
-            mostrel = key
+        avg1 = round(v1 / v2 , 2)
+        if avg1 >  mval[0]:
+            mval[0] = avg1
+            most[0] = key
         
         v1 , v2 = likeli[key]
-        avg2 = v1 / v2
-        avg1 = round(avg1,2)
-        avg2 = round(avg2 ,2)
+        avg2 = round(v1 / v2 , 2)
+        if avg2 >  mval[1]:
+            mval[1] = avg2
+            most[1] = key
+
         sum += abs(avg1 - avg2)
-        tot += 1
         cat1.append(avg1)
         cat2.append(avg2)
 
@@ -135,14 +156,16 @@ def index(request):
         'index':index,
         'continent':list(total_con.keys()),
         'values':list(total_con.values()),
-        'mostrel' : mostrel,
-        'relval':relval,
+        'most' : most,
+        'mval':mval,
         'year':list(year.keys()),
         'fyear':list(year.values()),
         'bflabel':(list(rel_like.keys()))[:5],
         'cat1':cat1[:5],
         'cat2':cat2[:5],
         'mean':round((sum/tot) , 2),
+        'ikey': list(intensity.keys()),
+        'ival':list(intensity.values()),
     }
 
     return render(request , 'index.html' , context=context)
