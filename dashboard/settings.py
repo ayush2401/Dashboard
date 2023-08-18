2c045adddae715977d75895bf5fa5c36563462c6
ayushsaraf <ayushsaraf.saraf15@gmail.com>
2023-08-19 01:27:26 +0530

    undo db

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": "mydatabase",
+#     }
+# }
 
 
 # Password validation
